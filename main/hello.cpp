6e1bcf3a443de78a725d1f8ea968510bf1523323
AdityaArakere <aditya.arakere@gmail.com>
2024-03-08 01:03:57 +0530

    commit 3

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "commiting a change to check webhook triggers\n";
+  cout << "commiting a change to check Jenkins pipelines\n";
   return 0;
 }
